5472d451555ef8a05f0b41eb14077f253419e428
ABDELILAHelkrymy <abdelilah.elkrymy@usmba.ac.ma>
2022-09-10 23:46:20 +0100

    Github Finder Project

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+import Footer from './components/layout/Footer';
+import Navbar from './components/layout/Navbar';
+import Alert from './components/layout/Alert';
+import {GithubProvider} from './context/github/GithubContext'
+import {AlertProvider} from './context/alert/AlertContext'
+import User from './pages/User';
+
+
+function App() {
+  return (
+    <GithubProvider>
+      <AlertProvider>
+        <Router>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar />
+            <main className='container mx-auto px-3'>
+              <Alert/>
+              <Routes>
+                <Route exact path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/user/:login' element={<User />} />
+                <Route path='/*' element={<NotFound />} />
+              </Routes>
+            </main>
+            <Footer/>
+          </div>
+        </Router>
+        </AlertProvider>
+    </GithubProvider>
+  );
+}
+
+export default App;
